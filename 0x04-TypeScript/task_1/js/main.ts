7086b79d46a62ca1f05d6a40e471e27d9551012f
abunusaybah <your_github_email@example.com>
2025-11-06 12:04:35 +0100

    Completed task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,4 +11,13 @@ interface Directors extends Teacher {
 	numberOfReports: number;
 }
 
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	return firstName.charAt(0) + "." + lastName;
+};
+
+console.log(printTeacher("AbdulMatiin", "Ismail"));
 // export {};
